3398b19e7391500dac59192cf484a46e528d6ba1
maya-nachiappan <maya.n102304@gmail.com>
2024-10-25 21:31:47 -0700

    Initial Commit

---
+++ customer structure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  customer structure
+//
+//  Created by maya nachiappan on 10/25/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
